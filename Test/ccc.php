04aa7889592e7617ddacb95c6be49b795508192c
ranmiaozai <ranmiaozai@MacBook-Pro-9.local>
2020-09-21 16:16:29 +0800

    1

--- Test/ccc.php
+++ Test/ccc.php
@@ -5,6 +5,6 @@ require $vendorPath . '/autoload.php';
 
 $Notice=new \Notice\Notice();
 $Notice->addDriver(new \Notice\Adapter\WeChat());
-var_dump($Notice->handle("通天之   哈哈哈哈"));
+var_dump($Notice->handle("启润之鉴黄宝典"));
 
 
