3ac7c50edf1071236a0b7ac3682e99bc0c2879da
hqr0597 <1094978933@qq.com>
2020-10-21 10:57:28 +0800

    去掉信息

--- test/demo.php
+++ test/demo.php
@@ -6,8 +6,5 @@ require $vendorPath . '/autoload.php';
 $Notice=new \Ranmiaozai\Notice\Notice();
 $Notice
     ->addDriver(new \Ranmiaozai\Notice\Adapter\DingTalk([
-        'content'=>'test',
-        'access_token'=>'9cd119bea0f59ae0c0daecd90ea1955037f7e810e74744f24d9eb712eafebedc',
-        'secret' => 'SEC7883eb4570335511e56ffe38b0f90946ef421f1312a41b3b0aee51aa5e37f993'
     ]))
     ->handle('test 456');
